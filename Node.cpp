f8da847d502a5f8a7e691f5d5af4432eea86ff5a
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2022-12-02 04:31:55 +0000

    Improved implementation of solution path

--- Node.cpp
+++ Node.cpp
@@ -1,21 +1,22 @@
+/**
+ * Description:
+ * this file implements methods defined in the Node.h file
+ *
+ * @author Richard Quayson
+ */
 
 #include "Node.h"
 
-Node::Node(int state, Node parent, double path_cost) {
+Node::Node(int state, Node *parent, double path_cost) {
     this->state = state;
-    this->parent = &parent;
+    this->parent = parent;
     this->pathCost = path_cost;
 }
 
-// overloaded constructor
 Node::Node(int state) {
     this->state = state;
     this->parent = nullptr;
     this->pathCost = 0.0;
-}
-
-Node::Node (void) {
-    this->parent = nullptr;
 }
 
 int Node::getState() const {
@@ -44,43 +45,21 @@ string Node::toString() {
     return node;
 }
 
-bool Node::operator== (Node &other) const {
+bool Node::operator== (const Node &other) const {
     return this->state == other.state;
 }
-
-//bool Node::equals(Node *n) {
-//    if (n == this) return true;
-//    if (!(instanceof<Node>(n))) {
-//        return false;
-//    }
-//    Node node = *dynamic_cast<Node>(*n);
-//    return this->getState() == node.getState();
-//}
 
 Solution Node::solutionPath() {
     vector<int> stateSequence;
     double totalPathCost = this->getPathCost();
-
-    stateSequence.insert(stateSequence.begin(), this->getState());
-    Node *node = this->getParent();
+    Node *node = this;
 
     while (node != nullptr) {
         stateSequence.insert(stateSequence.begin(), node->getState());
         node = node->getParent();
-
     }
 
     return {stateSequence, totalPathCost};
 }
 
-
-///**
-// * returns an int based on whether a node has a greater, equal or lower
-// * past cost than the node it is being compared with
-// * @param node the node to compare calling node object with
-// * @return int (1,-1,0)
-// */
-//    int compareTo(Node node) {
-//        return Double.compare(this.getPathCost(), node.getPathCost());
-//    }
- //ICP_INDIVIDUAL_PROJECT_C___NODE_H
+ ICP_INDIVIDUAL_PROJECT_C___NODE_H
