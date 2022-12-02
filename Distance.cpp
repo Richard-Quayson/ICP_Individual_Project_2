9615f2c4a50caede05d10ea3cb3a6a4a308bea45
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2022-12-02 04:30:30 +0000

    Second commit

--- Distance.cpp
+++ Distance.cpp
@@ -1,29 +1,30 @@
+/**
+ * Description:
+ * this file implements the haversine method defined in the Distance.h class
+ * the haversine method computes the distance between two places on the map
+ * given their longitudes and latitude values in degrees
+ *
+ * @author Paa Kwasi Kodua
+ */
 
 #include "Distance.h"
-#include <math.h>
-#include <algorithm> // for std::min
-#include <iostream>  // std::cout
 using namespace std;
 
-Distance::Distance(void) {}
+Distance::Distance() = default;
 
-Distance::~Distance(void) {}
+double Distance::haversine(double lat1, double lon1, double lat2, double lon2) const {
 
-double Distance::haversine(double lat1, double lon1, double lat2, double lon2){
-    // work in radians!
-    //(from R. W. Sinnott, "Virtues of the Haversine," Sky and Telescope, vol. 68, no. 2, 1984, p. 159):
-    double rlat1 = lat1*(pi()/180);
-    double rlat2 = lat2*(pi()/180);
-    double dlon = (lon2 - lon1) *(pi()/180);
-    double dlat = (lat2 - lat1)*(pi()/180);
-    double a = pow(sin(dlat/2.0),2.0) + cos(rlat1) * cos(rlat2) * pow(sin(dlon/2.0),2.0);
-    //double c = 2 * asin(min(one,sqrt(a)));
-    double c = 2 * atan2(sqrt(a),sqrt(1.0-a));
-    return earthradius() * c;
-}
+    // converting latitude and longitude values to radians
+    double sLat1 = lat1 * (pi / 180);
+    double sLat2 = lat2 * (pi / 180);
+    double dLon = (lon2 - lon1) * (pi / 180);
+    double dLat = (lat2 - lat1) * (pi / 180);
 
-double Distance::pythagores(double X1, double Y1, double X2,double Y2){
-    // ignoring elevation diffrence - the world is flat ...
-    double hypotenuse = sqrt(pow((X1 - X2),2) + pow((Y1 - Y2 ),2)  );
-    return hypotenuse;
+    // (source: https://en.wikipedia.org/wiki/Haversine_formula)
+    double a = pow(sin(dLat / 2.0),2.0) +
+            cos(sLat1) * cos(sLat2) * pow(sin(dLon/2.0),2.0);
+    double circumference = 2 * atan2(sqrt(a),sqrt(1.0 - a));
+
+    // distance in km
+    return earthRadius * circumference;
 }
--- Distance.h
+++ Distance.h
@@ -1,33 +1,45 @@
 /**
- * @author John Meehan
- * @source https://github.com/johnmeehan/Distance
- *
- * Description: Small class that provides approximate distance between
+ * Description:
+ * this class the provides approximate distance between
  * two points using the Haversine formula.
+ *
+ * @author Paa Kwasi Kodua
  */
 
 #ifndef ICP_INDIVIDUAL_PROJECT_C___HAVERSINE_H
 #define ICP_INDIVIDUAL_PROJECT_C___HAVERSINE_H
 
-//
-// Distance calculating class - John Meehan 2013
-//
-
-#include <math.h>
+#include <cmath>
 class Distance {
 public:
-    Distance(void);
-    virtual ~Distance(void);
+    /**
+     * Description:
+     * default constructor create Distance objects for
+     * accessing non-static methods of the distance class
+     */
+    Distance();
 
-    double haversine(double, double, double, double);
-    double pythagores(double, double, double, double);
-    //double polar_coordinate_flat_earth_formula(double, double, double, double);
+    /**
+     * Description:
+     * this method computes the distance between two places
+     * using their longitudes and latitudes
+     *
+     * @param lat1 the latitude in degrees of location 1
+     * @param lon1 the longitude in degrees of location 1
+     * @param lat2 the latitude in degrees of location 2
+     * @param lon2 the longitude in degrees of location 2
+     * @return double distance in km
+     */
+    double haversine(double lat1, double lon1, double lat2, double lon2) const;
 
 private:
-    inline double earthradius(){return 6378.137;}//KILOMETERS
-    //inline double pi(){ return atan(1)*4; }
-    inline double pi(){return 3.141592653589793238462; }
+    /**
+     * Description:
+     * constant instance variables storing the
+     * values of the radius of the earth and pi
+     */
+    const double earthRadius = 6378.137;
+    const double pi = 3.141592653589793238462;
 };
-
 
 #endif //ICP_INDIVIDUAL_PROJECT_C___HAVERSINE_H
