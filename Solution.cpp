aecfd8c7f98416bfeb78479cc14788b3e17e4b70
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2022-12-02 04:32:31 +0000

    Second commits

--- Solution.cpp
+++ Solution.cpp
@@ -1,8 +1,15 @@
+/**
+ * Description:
+ * this file implements methods defined in the Solution.h file
+ *
+ * @author Richard Quayson
+ */
 
 #include "Solution.h"
-#include <utility>
+#include "Route.h"
+#include "MainHelper.h"
 
-Solution::Solution() {}
+Solution::Solution() = default;
 
 Solution::Solution(vector<int> stateSequence, double pathCost) {
     this->stateSequence = std::move(stateSequence);
@@ -15,4 +22,41 @@ vector<int> Solution::getStateSequence() {
 
 double Solution::getPathCost() const {
     return pathCost;
+}
+
+/**
+ * Logic:
+ * loops through the state sequence of a given Solution object
+ * and retrieves the possible routes for the given state
+ * checks if a particular route has the same destination airport as the next state in the stateSequence
+ * if true, creates a string of the airlineCode, sourceAirportCode, destinationAirportCode
+ * for all states in the sequence and total flights, total additional stops,
+ * total distance and the optimality criteria
+ */
+string Solution::createSolutionString() {
+    string solutionString;
+    vector<int> actionSequence = this->getStateSequence();
+    int count = 0;
+    int totalStops = 0;
+    for (int airportId: actionSequence) {
+        for (Route route: MainHelper::routeData.at(airportId)) {
+            if (count < (actionSequence.size() -1)) {
+                if (route.getDestinationAirportId() == actionSequence[count + 1]) {
+                    solutionString += to_string(count+1) + ". " + route.getAirlineCode() + " from " +
+                            MainHelper::airportData.at(route.getSourceAirportId()).getIataCode() + " to " +
+                            MainHelper::airportData.at(route.getDestinationAirportId()).getIataCode() + " " +
+                            to_string(route.getStops()) + " stops.\n";
+                    count ++;
+                    totalStops += route.getStops();
+                }
+            }
+        }
+    }
+
+    solutionString += "\nTotal flights: " + to_string(actionSequence.size() - 1);
+    solutionString += "\nTotal additional stops: " + to_string(totalStops);
+    solutionString += "\nTotal distance: " + to_string(this->getPathCost()) + " km";
+    solutionString += "\nOptimality criteria: distance covered by route";
+
+    return solutionString;
 }
--- Solution.h
+++ Solution.h
@@ -1,22 +1,60 @@
+/**
+ * Description:
+ * this file defines operations for the Solution class
+ * operations include constructor and accessor methods,
+ * and a createSolutionString method to create summary string of the Solution
+ *
+ * @author Richard Quayson
+ */
 
 #ifndef ICP_INDIVIDUAL_PROJECT_C___SOLUTION_H
 #define ICP_INDIVIDUAL_PROJECT_C___SOLUTION_H
 
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class Solution {
+
 private:
+    // stores the sequence of states from the start airport to the destination airport
     vector<int> stateSequence;
-    double pathCost;
+
+    // stores the value of the path cost from the start airport to the destination airport
+    double pathCost{};
 
 public:
+    // default constructor for the Solution class
     Solution();
+
+    /**
+     * Constructor:
+     * Build and initialise the objects of the Solution class
+     *
+     * @param stateSequence the sequence of actions from the initial state to the goal state
+     * @param pathCost the distance between the stateSequence
+     */
     Solution(vector<int> stateSequence, double pathCost);
 
+    /**
+     * returns the state sequence of a given Solution object
+     * @return this.stateSequence
+     */
     vector<int> getStateSequence();
-    double getPathCost() const;
-};
 
+    /**
+     * returns the path cost of a given Solution object
+     * @return this.pathCost
+     */
+    double getPathCost() const;
+
+    /**
+     * Description:
+     * this methods create the summary to be written to the output file
+     * using the state sequence, the airportData and the routeData
+     * @return String
+     */
+    string createSolutionString();
+};
 
 #endif //ICP_INDIVIDUAL_PROJECT_C___SOLUTION_H
